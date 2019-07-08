5f1d0e9f38d8cf2407832f0c4e724045f121b593
soniakahlon <soniakahlon6344@gmail.com>
2019-07-08 12:30:16 -0400

    updated tables

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT || 9001,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://soniakahlon@localhost/housewivesserver',
   JWT_SECRET: process.env.JWT_SECRET || '123',
