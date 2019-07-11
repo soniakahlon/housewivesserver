111bb9ea8df6677e3e8c7e0a2b015fce3c9bc9fe
soniakahlon <soniakahlon6344@gmail.com>
2019-07-11 18:43:57 -0400

    users corrections

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -22,7 +22,7 @@ const AuthService = require('./auth-service')
                    .then(dbUser => {
                      if (!dbUser)
                        return res.status(400).json({
-                         error: 'Incorrect user_name or password',
+                         error: 'Incorrect username or password',
                        })
             
            
